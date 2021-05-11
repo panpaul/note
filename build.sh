4079cf35bc28044755478fb862949d1253cdc65e
Paul <1323564116@qq.com>
2021-05-11 10:10:13 +0800

    fix

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ rm -rf themes/next
 git clone https://github.com/next-theme/hexo-theme-next --branch v8.4.0 themes/next
 cp themes/_config.yml themes/next/
 sed -i "/commonweal/ a\  links: 友情链接" themes/next/languages/zh-CN.yml
-sed -i "/commonweal/ a\  links: 友情链接" themes/next/languages/en.yml
+sed -i "/commonweal/ a\  links: Links" themes/next/languages/en.yml
 npm i
 hexo generate
