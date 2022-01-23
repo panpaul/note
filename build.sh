05f3ca7a14c62e6573c1d53f975d8e29bc402bd7
Paul Pan <panyuxuan@hotmail.com>
2022-01-23 14:09:45 +0800

    update theme

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -x
 rm -rf themes/next
-git clone https://github.com/next-theme/hexo-theme-next --branch v8.8.2 themes/next
+git clone https://github.com/next-theme/hexo-theme-next --branch v8.9.0 themes/next
 cp themes/_config.yml themes/next/
 sed -i "/commonweal/ a\  links: 友情链接" themes/next/languages/zh-CN.yml
 sed -i "/commonweal/ a\  links: Links" themes/next/languages/en.yml
