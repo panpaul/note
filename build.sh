709c1fe650e9dc596fbb2ad7e020df1f3811e136
Paul Pan <panyuxuan@hotmail.com>
2022-05-09 12:14:51 +0800

    update deps

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 set -x
 rm -rf themes/next
-git clone https://github.com/next-theme/hexo-theme-next --branch v8.10.1 themes/next
+git clone https://github.com/next-theme/hexo-theme-next --branch v8.11.1 themes/next
 cp themes/_config.yml themes/next/
 cp themes/microsoft-clarity.njk themes/next/layout/_third-party/analytics/
 sed -i "/commonweal/ a\  links: 友情链接" themes/next/languages/zh-CN.yml
 sed -i "/commonweal/ a\  links: Links" themes/next/languages/en.yml
-yarn install
+npm install
 hexo generate
