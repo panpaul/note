74ea6af69d1fbf980cc4589d3291985c0a6f73a4
Paul Pan <i@0x7f.app>
2023-11-05 23:19:43 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ rm -rf themes/cactus
 git clone https://github.com/probberechts/hexo-theme-cactus.git themes/cactus
 cp themes/_config.yml themes/cactus/
 cp themes/diff.patch themes/cactus/
-pushd themes/cactus && git apply diff.patch && popd
+cd themes/cactus && git apply diff.patch && cd ..
 npm install
-hexo generate
+npm install hexo
+npx hexo generate
